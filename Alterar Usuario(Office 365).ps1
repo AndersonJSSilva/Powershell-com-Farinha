fd322d5f9a08f5c8b6836e49d8f3fe3412221d8c
AndersonJSSilva <105315956+AndersonJSSilva@users.noreply.github.com>
2025-05-01 10:56:24 -0300

    Add files via upload

---
+++ Alterar Usuario(Office 365).ps1
@@ -0,0 +1,25 @@
+﻿#Importação de Módulos Office 365
+$Credential=Get-Credential
+Import-Module ExchangeOnlineManagement
+Install-Module MSOnline
+Connect-MsolService -Credential $Credential
+
+#Limpeza de Variáveis
+$Usuarios = @();
+$Usuario = @();
+$Nome = @();
+$Sobrenome = @();
+$Cargo = @();
+$Email = @();
+
+#Entrada de Grupos
+$Usuarios = Get-Content -Path c:\temp\Usuarios.txt
+
+#Alteração de Usuários
+Foreach ($Usuario in $Usuarios){
+$Nome = @($Usuario -split ";")[0]
+$Sobrenome = @($Usuario -split ";")[1]
+$Cargo = @($Usuario -split ";")[2]
+$Email = @($Usuario -split ";")[3]
+Get-MSOLUser -UserPrincipalName $Email | Set-MSOLUser -FirstName $Nome -LastName $Sobrenome -Title $Cargo
+}
