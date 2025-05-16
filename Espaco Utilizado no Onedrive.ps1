949ab691b2d12a41d0350d6971feb2f1aec2c8c8
AndersonJSSilva <105315956+AndersonJSSilva@users.noreply.github.com>
2025-05-16 09:07:07 -0300

    Add files via upload

---
+++ Espaco Utilizado no Onedrive.ps1
@@ -0,0 +1,21 @@
+﻿param([string]$userName)
+
+Install-Module -Name Microsoft.Online.SharePoint.PowerShell
+Install-Module -Name MSOnline
+Install-Module -Name AzureAD
+Connect-MSOLService
+Connect-SPOService https://badesulcombr-admin.sharepoint.com
+
+$urlPrefix="https://badesulcombr-my.sharepoint.com/personal/"
+
+$url=$urlPrefix + $userName.Replace(".","_").Replace("@","_")
+
+$OneDrive=Get-SPOSite $url
+
+$ODBCurentUsage=$onedrive.StorageUsageCurrent
+
+$ODBStorageQuota=$OneDrive.StorageQuota
+
+$ODBLastContentModifiedDate=$OneDrive.LastContentModifiedDate
+
+Write-Host "User: $userName, Used Space $ODBCurentUsage, Quota $ODBStorageQuota, Last Changed date $ODBLastContentModifiedDate"
