a1da8e126ae3a4cf9a108a36a20cc1b376563e9d
AndersonJSSilva <105315956+AndersonJSSilva@users.noreply.github.com>
2025-06-25 18:24:38 -0300

    Add files via upload

---
+++ DesabilitarPOPIMAPSMTP.ps1
@@ -0,0 +1,14 @@
+﻿#Conectar Exchange Online
+Connect-ExchangeOnline
+
+#Desabilitar SMTP, POP e IMAP por Usuário
+Get-EXOCasMailbox -Filter {PrimarySmtpAddress -eq "Asilva.interop@es.sesc.com.br"} | Set-CASMailbox -ImapEnabled $false -PopEnabled $false -SmtpClientAuthenticationDisabled $true
+
+#Desabilitar SMTP, POP e IMAP para Todos
+Get-EXOCasMailbox -Filter {ImapEnabled -eq $True -or PopEnabled -eq $True} -ResultSize 5000 | Set-CASMailbox -ImapEnabled $false -PopEnabled $false -SmtpClientAuthenticationDisabled $true
+
+#Desabilitar POP e IMAP para Novos Usuários
+Get-CasMailboxplan | Set-CASMailboxPlan -ImapEnabled $false -PopEnabled $false
+
+#Desabilitar SMTP para Novos Usuários
+Set-TransportConfig -SmtpClientAuthenticationDisabled $true
